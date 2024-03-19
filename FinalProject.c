3d7469ac0a90bf0ea360fb9972d15720189fc0aa
ProgTayyab <151008155+ProgTayyab@users.noreply.github.com>
2024-03-19 19:52:08 +0000

    Push 2

--- FinalProject.c
+++ FinalProject.c
@@ -1,4 +1,46 @@
 #include<stdio.h>
+int Authentication();
+int Login();
+int SignUp();
+int CustomerLogin();
+int SellerLogin();
+
+
+
+
+
+
+
 int main(){
-    printf("hello");
+return 0;
+}
+/*Login(){
+    char user id
+} */
+SignUp(){
+
+char Name[20];
+char Email[20];
+int  NewPassword[6];
+int  RewritePassword[6];
+
+printf("Enter Your name : \n");
+scanf("%s",&Email);
+printf("Create your password   :must consist of a specialchar,digit,upper,lowercase character: \n");
+for(int i=0;i<6;i++){
+ scanf("%d",NewPassword[i]);   
+}
+printf("Enter Passoword again : \n");
+
+for(int i=0;i<6;i++){
+ scanf("%d",RewritePassword[i]);   
+}
+
+for(int i=0;i<6;i++){
+if{
+    NewPassword[i]==RewritePassword[i];
+}
+
+}
+
 }
